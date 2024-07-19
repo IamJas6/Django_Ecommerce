f57639c7836274dbc941807c4eddbd5e55ce059e
IamJas6 <suhailuddin2002@gmail.com>
2024-07-19 19:09:51 +0530

    password added

--- my_ecom/settings.py
+++ my_ecom/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'sxJOpswtzSHYEbZhmRlYExCnvhPqYynE',
+        'PASSWORD': 'os.environ['DB_PASS_CODE']',
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '23844',
     }
