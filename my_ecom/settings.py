fef8707ef6752c7e9ec1741fd8863de0764d6574
IamJas6 <suhailuddin2002@gmail.com>
2024-07-19 19:26:51 +0530

    db pass added

--- my_ecom/settings.py
+++ my_ecom/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'os.environ['DB_PASS_CODE']',
+        'PASSWORD': os.environ['DB_PASS_CODE'],
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '23844',
     }
